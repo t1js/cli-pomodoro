543d6ad09046e6080c57771f53fe45344c2b67fb
t1js <97554484+t1js@users.noreply.github.com>
2024-05-09 14:59:53 +0200

    working flow

--- pomodoro.js
+++ pomodoro.js
@@ -1,1 +1,43 @@
-console.log("hi")
+const readline = require('readline');
+
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface( { input, output });
+
+const workDuration = 25;
+const shortBreakDuration = 5;
+const longBreakDuration = 25;
+const pomodoros = 4;
+
+
+function startPomodoro() {
+    console.log("Timer started. Let's do this!");
+
+for (let i = 1; i < pomodoros; i++) {
+    setTimeout(() => {
+        console.log("Cycle " + i + " is finised! Let's have a break.");
+    }, (workDuration * 60 * 1000 * i) + (shortBreakDuration * 60 * 1000 * (i - 1)));
+
+    setTimeout(() => {
+        console.log("Time is up! Let's get to work.")
+    }, (workDuration * 60 * 1000 * i) + (shortBreakDuration * 60 * 1000 * i));
+};
+
+setTimeout(() => {
+    console.log("You did 4 amazing pomodoro cycles and deserve some rest. We'll let you know when you can get back to work.");
+    setTimeout(() => {
+        console.log("Your long break is done. Restart the timer for another pomodoro session.");
+        rl.close();
+    }, longBreakDuration * 60 * 1000);
+}, (workDuration * 60 * 1000 * pomodoros) + (shortBreakDuration * 60 * 1000 * (pomodoros - 1)));
+}
+
+rl.question('Press P (for pomodoro) to start timer: ', (answer) => {
+
+    if (answer === "P" || answer === "p") {
+        startPomodoro();
+    } else {
+        console.log("Unsupported input, try again.");
+        rl.close();
+    }
+})
